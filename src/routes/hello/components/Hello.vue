d5f74a7931b23177f8bfc042bd2724ddb8134006
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-10-03 16:19:15 +0200

    feat(feature1): add link to homepage

--- src/routes/hello/components/Hello.vue
+++ src/routes/hello/components/Hello.vue
@@ -17,8 +17,13 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <hr style="width: 40%" />
+    <ul>
+      <li>
+        <router-link to="feature1">Feature1 demonstration</router-link>
+      </li>
+    </ul>
   </div>
-
 </template>
 
 <script>
