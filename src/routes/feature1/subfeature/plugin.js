bfb2cd4869fc76419be02c4ee244ebff9d281c2e
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-10-03 16:39:04 +0200

    refactor(subfeature): add noop instead of console

--- src/routes/feature1/subfeature/plugin.js
+++ src/routes/feature1/subfeature/plugin.js
@@ -1,3 +1,5 @@
+import { noop } from 'ramda-adjunct';
+
 import module from './module';
 
 // this can be done better in the future when following issue is resolved:
@@ -7,9 +9,7 @@ const subfeaturePlugin = ({ namespace = 'feature1/subfeature' } = {}) => (store)
   store.registerModule(namespace, module);
 
   // this is the place where we handle our complex side effects
-  store.subscribe((mutation) => {
-    console.dir(mutation);
-  });
+  store.subscribe(noop);
 };
 
 
